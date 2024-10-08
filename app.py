6b1f7c88d88f8c452a131b1e15df2ae199c0c3c0
jungminii <127848065+jungminii@users.noreply.github.com>
2024-10-08 16:46:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def downwardApi():
-    pod_name = os.getenv('POD_NAME', '알 수 없는 Pod')
-    node_name = os.getenv('NODE_NAME', '알 수 없는 Node')
-    pod_namespace = os.getenv('POD_NAMESPACE', '알 수 없는 Namespace')
+# Downward API를 통해 전달된 환경 변수에서 Pod의 이름, 노드 이름, 네임스페이스를 가져옴.
+@app.route('/') # 루트 디렉토리에 접근시에 호출되는 메소드
+def pod_info():
+    pod_name = os.getenv('POD_NAME', 'Unknown')
+    node_name = os.getenv('NODE_NAME', 'Unknown')
+    pod_namespace = os.getenv('POD_NAMESPACE', 'Unknown')
     
-    return f"Container EDU | POD 이름: {pod_name} | 노드 이름: {node_name} | 네임스페이스: {pod_namespace}"
+    return jsonify({
+        'pod_name': pod_name,
+        'node_name': node_name,
+        'pod_namespace': pod_namespace
+    })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=8080) # 모든 네트워크 인터페이스에서 접근 가능하게 하고, 8080 포트에서 서버 실행
